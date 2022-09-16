e46e54b827106db1b1c7d68a8917a62125d22a20
CodingIsaac <isaacijuo@gmail.com>
2022-09-16 12:57:42 +0000

    EIP-1822-Upgradable-Contract Initial Commit

---
+++ adeFunke.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "./proxiable.sol";
+
+contract Funke is Proxiable {
+
+    uint256 TotalSupply;
+    string Name;
+    string Symbol;
+    bool public initialized;
+    address public owner;
+
+    
+
+
+}
